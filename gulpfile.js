3c960ad240a731006ce4936f46aa54e77582e126
Dan Vanderkam <danvdk@gmail.com>
2015-03-10 13:15:01 -0400

    yeoman react-gulp-browserify

---
+++ gulpfile.js
@@ -0,0 +1,135 @@
+'use strict';
+
+var gulp = require('gulp');
+var del = require('del');
+
+
+
+// Load plugins
+var $ = require('gulp-load-plugins')();
+var browserify = require('browserify');
+var watchify = require('watchify');
+var source = require('vinyl-source-stream'),
+    
+    sourceFile = './app/scripts/app.js',
+    
+    destFolder = './dist/scripts',
+    destFileName = 'app.js';
+
+
+
+// Scripts
+gulp.task('scripts', function () {
+    var bundler = watchify(browserify({
+        entries: [sourceFile],
+        insertGlobals: true,
+        cache: {},
+        packageCache: {},
+        fullPaths: true
+    }));
+
+    bundler.on('update', rebundle);
+
+    function rebundle() {
+        return bundler.bundle()
+            // log errors if they happen
+            .on('error', $.util.log.bind($.util, 'Browserify Error'))
+            .pipe(source(destFileName))
+            .pipe(gulp.dest(destFolder));
+    }
+
+    return rebundle();
+
+});
+
+
+
+
+
+// HTML
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'))
+        .pipe($.size());
+});
+
+// Images
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe($.size());
+});
+
+
+
+// Clean
+gulp.task('clean', function (cb) {
+    cb(del.sync(['dist/styles', 'dist/scripts', 'dist/images']));
+});
+
+
+// Bundle
+gulp.task('bundle', ['scripts', 'bower'], function(){
+    return gulp.src('./app/*.html')
+               .pipe($.useref.assets())
+               .pipe($.useref.restore())
+               .pipe($.useref())
+               .pipe(gulp.dest('dist'));
+});
+
+// Webserver
+gulp.task('serve', function () {
+    gulp.src('./dist')
+        .pipe($.webserver({
+            livereload: true,
+            port: 9000
+        }));
+});
+
+// Bower helper
+gulp.task('bower', function() {
+    gulp.src('app/bower_components/**/*.js', {base: 'app/bower_components'})
+        .pipe(gulp.dest('dist/bower_components/'));
+
+});
+
+gulp.task('json', function() {
+    gulp.src('app/scripts/json/**/*.json', {base: 'app/scripts'})
+        .pipe(gulp.dest('dist/scripts/'));
+});
+
+// Robots.txt and favicon.ico
+gulp.task('extras', function () {
+    return gulp.src(['app/*.txt', 'app/*.ico'])
+        .pipe(gulp.dest('dist/'))
+        .pipe($.size());
+});
+
+// Watch
+gulp.task('watch', ['html', 'bundle', 'serve'], function () {
+
+    // Watch .json files
+    gulp.watch('app/scripts/**/*.json', ['json']);
+
+    // Watch .html files
+    gulp.watch('app/*.html', ['html']);
+
+    
+
+
+
+    // Watch image files
+    gulp.watch('app/images/**/*', ['images']);
+});
+
+// Build
+gulp.task('build', ['html', 'bundle', 'images', 'extras']);
+
+// Default task
+gulp.task('default', ['clean', 'build']);
